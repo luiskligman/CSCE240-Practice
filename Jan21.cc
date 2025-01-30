10db20f5c9d8cdadc3b325a1ad8dc725749bd80f
Luis Kligman <137526222+luiskligman@users.noreply.github.com>
2025-01-29 21:54:18 -0500

    Add files via upload

---
+++ Jan21.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+    int x;
+    cout << "Sup, world?" << endl;
+    cout << "x = " << x << endl;
+
+    int y = 11;
+    cout << y << " / 3 = " << y / 3.0 << endl;
+
+    cout << "11 % 3 = " << 11 % 3 << endl;
+
+    int i = 10;
+    cout << i++ + i << endl;
+    cout << i << endl;
+
+    return 0;
+}
